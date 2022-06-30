7ed45b9fa44163d47d8e1bd0109e8d773d0d8d48
mobilepagetest <hello@mobilepagetest.de>
2022-06-30 03:17:26 +0200

    Angular CRUID

---
+++ src/app/fruits/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {Fruits} from "../fruits";
+import {FruitsService} from "../fruits.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  allFruits : Fruits[] = [];
+  constructor(private fruitService : FruitsService) { }
+
+  ngOnInit(): void {
+    this.get()
+  }
+
+  get() {
+    this.fruitService.get().subscribe((data) => {
+      this.allFruits = data;
+    })
+  }
+
+}
