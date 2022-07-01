acbd24f3f7dc2d5a9e81e6430054c6efdcfe4270
mobilepagetest <hello@mobilepagetest.de>
2022-07-01 07:04:39 +0200

    Angular CRUID

--- src/app/fruits/fruits.module.ts
+++ src/app/fruits/fruits.module.ts
@@ -5,12 +5,14 @@ import { FormsModule} from "@angular/forms";
 import { FruitsRoutingModule } from './fruits-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    CreateComponent
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     CommonModule,
--- src/app/fruits/fruits.service.ts
+++ src/app/fruits/fruits.service.ts
@@ -16,4 +16,12 @@ export class FruitsService {
   create(payload: Fruits) {
     return this.http.post<Fruits>('http://localhost:3000/fruits', payload);
   }
+
+  getById(id :number) {
+    return this.http.get<Fruits>('http://localhost:3000/fruits/$:{id}');
+  }
+
+  update(payload : Fruits) {
+    return this.http.put<Fruits>('http://localhost:3000/fruits/$:{payload.id}',payload);
+  }
 }
